6274a4f29b2a1eb2394bc614a300ac0eb2bdd0d2
dream-codee <53904419+dream-codee@users.noreply.github.com>
2021-04-12 10:34:04 +0530

    seventh commit

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def view1(request):
+    return HttpResponse("<h1>Hello world</h1>")
+
+def view2(request,email):
+    return render(request,"view2.html",context={'email':email,'name':"Akshay"})
+
+def view3(request,name):
+    return HttpResponse(f'<h1>Hello Mr./Ms. {name}</h1>')
+
+
+# Create your views here.
