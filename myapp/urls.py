aad0bda8f48e333eed0f2b3b8be9d3f9cfdd80f1
dream-codee <53904419+dream-codee@users.noreply.github.com>
2021-04-14 13:04:19 +0530

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('view1/',views.view1,name="view1"),
     path('view2/<email>',views.view2,name="view2"),
     path('view3/<name>',views.view3,name="view3"),
+    path('',views.if_demo,name="if_demo"),
+    path('if/',views.ifelse_demo,name="ifelse_demo"),
+    path('for/',views.for_demo,name="for_demo"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,10 +7,21 @@ def view1(request):
     return HttpResponse("<h1>Hello world</h1>")
 
 def view2(request,email):
-    return render(request,"view2.html",context={'email':email,'name':"Akshay"})
+    return render(request,"view2.html",context={'email':email,'name':"SABA"})
 
 def view3(request,name):
     return HttpResponse(f'<h1>Hello Mr./Ms. {name}</h1>')
 
+def if_demo(request):
+    login=True
+    return render(request,"if_demo.html",context={'login':login})
+
+def ifelse_demo(request):
+    login=True
+    return render(request,"ifelse_demo.html",context={'login':login,'name':'SABA','a':10,'b':50})
+
+def for_demo(request):
+    return render(request,"for_demo.html",context={"items":['apple','ball','cat'],\
+        'profile':{'name':"saba","age":23,"sal":10}})
 
 # Create your views here.
